2e203444703c089e4a65ea1e4c7c2bb66b7814cf
alir3zakh <kheradalireza@gmail.com>
2022-11-02 10:27:55 +0100

    examples - adding dynamic content to component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,15 @@
 
-const App = () => (
-  <div>
-    <p>Hello World</p>
-  </div>
-)
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+
+  return (
+    <div>
+      <p>Hello World, it is {now.toString()}</p>
+      <p>{a} plus {b} is {a + b}</p>
+    </div>
+  )
+}
 
 export default App;
