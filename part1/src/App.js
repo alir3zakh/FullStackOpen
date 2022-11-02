d560c9500650be611c3cfecf2fd9bb97dbdce82d
alir3zakh <kheradalireza@gmail.com>
2022-11-02 10:33:01 +0100

    examples - Multiple Components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,13 +1,17 @@
-
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello World, it is {now.toString()}</p>
-      <p>{a} plus {b} is {a + b}</p>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
     </div>
   )
 }
