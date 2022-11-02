954a3eff90828bc96db4ee5147d29e902ce65e9a
alir3zakh <kheradalireza@gmail.com>
2022-11-02 11:00:49 +0100

    Exercise 1.1 - Total Component Added

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,6 +14,14 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  return(
+    <p>
+      Number of exercises {props.total}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -28,7 +36,7 @@ const App = () => {
       <Header course={course} />
       <Content parts={[part1, part2, part3]}
         exercises={[exercises1, exercises2, exercises3]} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
