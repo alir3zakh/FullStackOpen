7f4eee72292bcc5cc386aa15621225204fb121fc
alir3zakh <kheradalireza@gmail.com>
2022-11-02 10:36:39 +0100

    examples - Adding props to components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,19 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 
 const App = () => {
+  let name = 'Alireza';
+  let age = 24;
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
+      <Hello name={name} age={age} />
     </div>
   )
 }
