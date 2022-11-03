398868e7200aceb7a39fbd7749031eede9a8e682
alir3zakh <kheradalireza@gmail.com>
2022-11-03 17:17:16 +0100

    Part 1.c - Simple component state & event handlers

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,57 +1,27 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return <p>{props.part} {props.exercise}</p>
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  )
-}
+import { useState } from "react"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [counter, setCounter] = useState(0);
+
+  // event handlers
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <>
+      <Display counter={counter} />
+      <Button text="plus" onClickHandler={increaseByOne} />
+      <Button text="decrease" onClickHandler={decreaseByOne} />
+      <Button text="reset" onClickHandler={reset} />
+    </>
   )
+
 }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ text, onClickHandler }) =>
+  <button onClick={onClickHandler}>{text}</button>
 
 export default App;
