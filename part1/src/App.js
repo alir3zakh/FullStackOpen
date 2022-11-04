3312a819a2dbc38fb50292401fb734833e92e663
alir3zakh <kheradalireza@gmail.com>
2022-11-04 03:26:49 +0100

    E1.6 - unicafe step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,11 +6,55 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // handler functions
+  const setGoodHandler = () => setGood(good + 1)
+  const setNeutralHandler = () => setNeutral(neutral + 1)
+  const setBadHandler = () => setBad(bad + 1)
+
   return (
-    <div>
-      code here
+    <div id="unicafe">
+      <Feedback title="Give Feedback"
+        goodHandler={setGoodHandler}
+        neutralHandler={setNeutralHandler}
+        badHandler={setBadHandler} />
+
+      <Stats title="Statistics"
+        state={{ good: good, neutral: neutral, bad: bad }} />
     </div>
   )
 }
+
+const Stats = ({ title, state }) => {
+  return (
+    <div id="stats">
+      <DisplayTitle title={title} />
+      <SingleStat param="good" val={state.good} />
+      <SingleStat param="neutral" val={state.neutral} />
+      <SingleStat param="bad" val={state.bad} />
+
+    </div>
+  )
+}
+
+const SingleStat = ({ param, val }) => (
+  <>{param}: {val} <br /></>
+)
+
+const Feedback = (props) => {
+  return (
+    <div id="feedback">
+      <DisplayTitle title={props.title} />
+      <Button text="good" handler={props.goodHandler} />
+      <Button text="neutral" handler={props.neutralHandler} />
+      <Button text="bad" handler={props.badHandler} />
+    </div>
+  )
+}
+
+const Button = ({ text, handler }) => (
+  <button onClick={handler}>{text}</button>
+)
+
+const DisplayTitle = ({ title }) => <h2>{title}</h2>
 
 export default App
