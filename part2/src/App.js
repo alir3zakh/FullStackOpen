e64e483f5aeb1607e2af1f3981bb3f307c1fdd02
alir3zakh <kheradalireza@gmail.com>
2022-11-06 01:13:24 +0100

    Adding form to Notes App

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,59 +1,48 @@
-import Course from './components/Course'
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('initial note state')
+
+  const addNote = (event) => {
+    event.preventDefault();
+
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
     }
-  ]
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const changeNoteHandle = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
-    <div id="course-wrapper">
-      {courses.map(course =>
-        <Course key={course.id}
-          name={course.name}
-          parts={course.parts} />
-      )}
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id}
+            note={note}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={changeNoteHandle}
+        />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
-export default App;
+
+export default App
