ef07567c501b12f180fa845bfd4597baab641d6e
alir3zakh <kheradalireza@gmail.com>
2022-11-13 11:33:57 +0100

    Ex - Notes: Footer Component added with inline styles

--- Notes/src/App.js
+++ Notes/src/App.js
@@ -97,6 +97,23 @@ const App = () => {
         />
         <button type='submit'>save</button>
       </form>
+
+      <Footer />
+    </div>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
     </div>
   )
 }
