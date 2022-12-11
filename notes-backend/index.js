da658eec27dd0033143bd1053cb8aae9c98c5df1
alir3zakh <kheradalireza@gmail.com>
2022-12-11 13:40:59 +0100

    Ex - Notes: using database in get & post route handlers

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -12,33 +12,7 @@ app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    }
-]
 
-const generateID = () => {
-    const maxID = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxID + 1
-}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -51,18 +25,12 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(n => n.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.body.id).
+        then(note => response.json(note))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+
 
     response.status(204).end()
 })
@@ -76,16 +44,15 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        id: generateID(),
         date: new Date(),
         important: body.important || false
-    }
+    })
 
-    notes.push(note)
-    response.json(note)
-    console.log(notes)
+    note.save().then(saved_note => {
+        response.json(saved_note)
+    })
 })
 
 const PORT = process.env.PORT
