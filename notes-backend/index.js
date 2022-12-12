7aed71ec5daa425666961de8e71006e4d37cec00
alir3zakh <kheradalireza@gmail.com>
2022-12-12 20:29:47 +0100

    3.22 - ESlint warnings fixed

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -16,86 +16,85 @@ app.use(express.json())
 const baseURL = '/api/notes/'
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get(baseURL, (req, res) => {
-    Note.find({}).then(notes => {
-        res.json(notes)
-    })
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.get(`${baseURL}:id`, (req, res, next) => {
-    Note.findById(req.params.id)
-        .then(note => {
-            if (note) {
-                res.json(note)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            next(error)
-        })
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
 })
 
 app.delete(`${baseURL}:id`, (req, res, next) => {
-    Note.findByIdAndRemove(req.params.id)
-        .then(result => {
-            // 204: no content
-            res.status(204).end()
-        })
-        .catch(error => naxt(error))
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      // 204: no content
+      res.status(204).end()
+    })
+    .catch(error => naxt(error))
 })
 
 app.post(baseURL, (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const note = new Note({
-        content: body.content,
-        date: new Date(),
-        important: body.important || false
-    })
+  const note = new Note({
+    content: body.content,
+    date: new Date(),
+    important: body.important || false
+  })
 
-    note.save()
-        .then(saved_note => res.json(saved_note))
-        .catch(err => next(err))
+  note.save()
+    .then(saved_note => res.json(saved_note))
+    .catch(err => next(err))
 })
 
 app.put(`${baseURL}:id`, (req, res, next) => {
-    const { content, important } = req.body
+  const { content, important } = req.body
 
-    Note.findByIdAndUpdate(
-        req.params.id,
-        { content, important },
-        { new: true, runValidators: true, context: 'query' })
-        .then(updated_note => res.json(updated_note))
-        .catch(error => next(error))
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updated_note => res.json(updated_note))
+    .catch(error => next(error))
 })
 
 // req handler for unknown endpoint
 // ** HAS TO BE NEXT TO LAST
 app.use((req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 })
 
 
 // error handler
 // ** HAS TO BE LAST
 app.use((err, req, res, next) => {
-    console.log(err.message)
+  console.log(err.message)
 
-    if (err.name === 'CastError') {
-        return res.status(400)
-            .send({ error: 'malformatted id' })
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).json({ error: err.message })
-    }
-    // pass the error to default Express error handler
-    next(err)
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+  // pass the error to default Express error handler
+  next(err)
 })
--- notes-backend/mongo.js
+++ notes-backend/mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,23 +11,23 @@ const password = process.argv[2]
 const uri = `mongodb+srv://alir3zakh:${password}@cluster0.5vugccj.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 mongoose
-    .connect(uri)
-    .then((result) => {
-        console.log('connected')
+  .connect(uri)
+  .then(() => {
+    console.log('connected')
 
-        Note.find({}).then(result => {
-            result.forEach(note => {
-                console.log(note)
-            })
-            mongoose.connection.close()
-        })
+    Note.find({}).then(result => {
+      result.forEach(note => {
+        console.log(note)
+      })
+      mongoose.connection.close()
     })
-    .catch((err) => console.log(err))
+  })
+  .catch((err) => console.log(err))
