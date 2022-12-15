5b72a062aa65f217d910592e7cf49288e9b16d4c
alir3zakh <kheradalireza@gmail.com>
2022-12-15 15:07:42 +0100

    Ex - Notes_backend: updated project directory structure

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,48 +1,10 @@
-const config = require('./utils/config')
 const logger = require('./utils/logger')
+const app = require('./app')
+const config = require('./utils/config')
+const http = require('http')
 
-const express = require('express')
-const app = express()
-
-const cors = require('cors')
-const notesRouter = require('./controllers/note')
-
-
-// allow for requests from all origins
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-// app.use(middleware.requestLogger)
-
-app.use('/api/notes', notesRouter)
+const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
-})
-
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello World!</h1>')
-// })
-
-
-
-// req handler for unknown endpoint
-// ** HAS TO BE NEXT TO LAST
-app.use((req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-})
-
-
-// error handler
-// ** HAS TO BE LAST
-app.use((err, req, res, next) => {
-  logger.error(err.message)
-
-  if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (err.name === 'ValidationError') {
-    return res.status(400).json({ error: err.message })
-  }
-  // pass the error to default Express error handler
-  next(err)
 })
--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
-
-// connecting to database
-mongoose.connect(url)
-  .then(result => console.log('connected to MongoDB'))
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
