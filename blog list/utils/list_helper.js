bfe6a697048afeb230947654d1d8e5596653361f
alir3zakh <kheradalireza@gmail.com>
2022-12-16 16:18:21 +0100

    4.5 - Blog List: Added unit tests for favorite blog

--- blog list/utils/list_helper.js
+++ blog list/utils/list_helper.js
@@ -11,8 +11,21 @@ const totalLikes = (blogs) => {
     0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0)
+    return 0
+  
+  const unwrap = ({ title, author, likes }) =>
+    ({ title, author, likes });
+
+  const max_like = Math.max(...blogs.map(b => b.likes))
+  const blog = blogs.find(b => b.likes === max_like)
+  return unwrap(blog)
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
