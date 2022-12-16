bd09e16354df26a8d071c335c40fca52e4260bb0
alir3zakh <kheradalireza@gmail.com>
2022-12-16 20:30:08 +0100

    Ex - notes_backend: refactored routes to async/await syntax + more tests

--- notes-backend/controllers/note.js
+++ notes-backend/controllers/note.js
@@ -1,33 +1,30 @@
 const Note = require('../models/note')
 const notesRouter = require('express').Router()
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes)
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then(note => {
-      if (note) {
-        res.json(note)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
 })
 
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).end() // 204: no content
-    })
-    .catch(error => naxt(error))
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const _ = await Note.findByIdAndRemove(req.params.id)
+    res.status(204).end() // 204: no content
+  } catch (error) {
+    naxt(error)
+  }
 })
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body
 
   const note = new Note({
@@ -36,21 +33,21 @@ notesRouter.post('/', (req, res, next) => {
     important: body.important || false
   })
 
-  note.save()
-    .then(saved_note => res.json(saved_note))
-    .catch(err => next(err))
+  const saved_note = await note.save()
+  res.status(201).json(saved_note)
 })
 
 
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
   const { content, important } = req.body
 
-  Note.findByIdAndUpdate(
+  const updated_note = await Note.findByIdAndUpdate(
     req.params.id,
     { content, important },
-    { new: true, runValidators: true, context: 'query' })
-    .then(updated_note => res.json(updated_note))
-    .catch(error => next(error))
+    { new: true, runValidators: true, context: 'query' }
+  )
+
+  res.json(updated_note)
 })
 
 module.exports = notesRouter
