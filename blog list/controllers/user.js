eb1adb67d8d94b07e8b907a8a28bc44836b21680
alir3zakh <kheradalireza@gmail.com>
2022-12-20 23:42:08 +0100

    4.15 - Blog List: Added basic user management

---
+++ blog list/controllers/user.js
@@ -0,0 +1,31 @@
+const User = require('../models/user')
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+usersRouter.post('/', async (req, res) => {
+  const { username, name, password } = req.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  // const existingUser = await User.findOne({ username })
+  // if (existingUser) {
+  //   return res.status(400).json({
+  //     error: 'username must be unique'
+  //   })
+  // }
+
+  const user = new User({
+    username, name, passwordHash
+  })
+
+  const savedUser = await user.save()
+  res.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
--- blog list/index.js
+++ blog list/index.js
@@ -5,6 +5,6 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-app.listen(config.PORT, () => {
-    logger.info('Server Running on port ', config.PORT)
+server.listen(config.PORT, () => {
+    logger.info('Server Running on port', config.PORT)
 })
