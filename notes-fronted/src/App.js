0bfffb28f3737335494843b77e1b4eee44cbe300
alir3zakh <kheradalireza@gmail.com>
2022-12-23 03:34:20 +0100

    Ex - notes: Added login functionality + renamed directories

--- notes-fronted/src/App.js
+++ notes-fronted/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification';
 import noteService from './services/note'
+import loginService from './services/login'
 
 const App = () => {
   //component states
@@ -10,6 +11,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     const dummyNote = {
@@ -40,7 +44,23 @@ const App = () => {
         setNotes(notes.concat(newNote))
         setNewNote('')
       })
+  }
 
+  const loginHandler = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log(error)
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   const toggleImportanceOf = (id) => {
@@ -72,10 +92,48 @@ const App = () => {
     notes :
     notes.filter(note => note.important))
 
+  const loginForm = () => (
+    <form onSubmit={loginHandler}>
+      <div>
+        username:
+        <input value={username} name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password:
+        <input value={password} name="Password" type="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='Submit'>Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={changeNoteHandle}
+      />
+      <button type='submit'>save</button>
+    </form>
+  )
+
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      }
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -90,18 +148,11 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={changeNoteHandle}
-        />
-        <button type='submit'>save</button>
-      </form>
-
       <Footer />
     </div>
   )
 }
+
 
 const Footer = () => {
   const footerStyle = {
