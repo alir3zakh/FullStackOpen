d641e54fab04b82f4ff92e2f755f601c609bfa2f
alir3zakh <kheradalireza@gmail.com>
2022-12-28 23:41:24 +0100

    5.7 - Bloglist_frontend: Added button for toggling view of blog details

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [display, setDisplay] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const displayButton = () => (
+    <button onClick={() => setDisplay(!display)}>
+      {display ? 'hide' : 'view'}
+    </button>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {display ?
+        <>
+          {blog.title} {displayButton()} <br />
+          {blog.url} <br />
+          {blog.likes} <button>like</button><br />
+          {blog.author}
+        </> :
+        <>
+          {blog.title} {blog.author} {displayButton()}
+        </>
+      }
+      {' '}
+
+    </div>
+  )
+}
 
 export default Blog
